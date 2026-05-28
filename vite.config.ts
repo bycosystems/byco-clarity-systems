ddf35ec1f296244f7661f6e844b6c2fc7a60ac35
bycosystems <contact.byco@gmail.com>
2026-05-28 15:34:29 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
-  base: '/',
+  base: '/byco-clarity-systems/',
   tanstackStart: {
     server: { entry: "server" },
   },
